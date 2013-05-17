5ad6be941dfba2766fada97494af9373de67a27a
bloomingbridges <brueckner.florian@googlemail.com>
2013-05-17 13:16:35 +0100

    Screengrab watcher implemented.

--- TRANS_CUBUS/churn.js
+++ TRANS_CUBUS/churn.js
@@ -1,6 +1,32 @@
 
-var dgram = require('dgram')
+// Modules /////////////////////////////////////////////////////////////////////
+
+var dgram = require('dgram'),
+	watchr = require('watchr');
+
+// Globals /////////////////////////////////////////////////////////////////////
+
 var udp = dgram.createSocket('udp4');
+
+// Watchr //////////////////////////////////////////////////////////////////////
+
+watchr.watch({
+	path: "../IN_COMING/",
+	listeners: {
+        error: function(err){
+            console.log('Watchr ERROR: ', err);
+        },
+        change: function(changeType,filePath,fileCurrentStat,filePreviousStat){
+            if (changeType === "create" || changeType === "update") {
+            	var index = filePath.match(/([0-360])/)[1];
+            	console.log("FRESH [" + index + "]");
+            }
+            //console.log('CHANGE: ', arguments);
+        }
+	}
+});
+
+// UDP /////////////////////////////////////////////////////////////////////////
 
 udp.on("message", function (msg, rinfo) {
   console.log("Receiving: " + msg + " from " +
