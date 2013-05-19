f71e0be205b34d283492ac1d1daadb0679b500aa
bloomingbridges <brueckner.florian@googlemail.com>
2013-05-19 18:58:59 +0100

    Added credentials file + loading.

--- EX_CUBUS/app.js
+++ EX_CUBUS/app.js
@@ -1,17 +1,38 @@
 
-var express = require('express');
-var app = express();
+var express = require('express'),
+	fs = require('fs');
 
-app.set('title', 'IN//CUBUS');
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.logger('dev'));
-app.use(express.static(__dirname + '/public'));
+var app = express(),
+	credentials = {};
 
-app.get('/', function(req,res) {
-	res.render('index',
-			{ title: app.get('title') }
-		);
+fs.readFile(__dirname + '/credentials.json', 'utf8', function(error, data) {
+	if (!error) {
+		credentials = JSON.parse(data);
+		setupExpressApp();
+	}
+	else
+		console.log(error);
 });
 
-app.listen(process.env.PORT);
+
+function setupExpressApp() {
+
+	app.set('title', 'IN//CUBUS');
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.logger('dev'));
+	app.use(express.static(__dirname + '/public'));
+
+	app.get('/', function(req,res) {
+		res.render('index',
+				{ title: app.get('title') }
+			);
+	});
+
+	var port = (process.env.PORT) ? process.env.PORT : 3000;
+
+	app.listen(port);
+
+}
+
+
